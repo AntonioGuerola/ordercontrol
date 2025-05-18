933164437d4cd314b358f3bea8817501fb18923c
Antonio Guerola Crespo <antonioguerolacrespo@gmail.com>
2025-05-18 16:02:05 +0200

    Implementación DTO para Usuario

---
+++ backend/src/main/java/com/antonio/ordercontrol/mappers/MesaMapper.java
@@ -0,0 +1,25 @@
+package com.antonio.ordercontrol.mappers;
+
+import com.antonio.ordercontrol.dtos.MesaDTO;
+import com.antonio.ordercontrol.models.Mesa;
+
+public class MesaMapper {
+    public static MesaDTO toMesaDTO(Mesa mesa) {
+        return new MesaDTO(
+                mesa.getId().longValue(),
+                mesa.getTipo().getNombre(),
+                mesa.getNumMesa(),
+                mesa.getEstado(),
+                mesa.getFechaHora()
+        );
+    }
+
+    public static Mesa toMesa(MesaDTO mesaDTO) {
+        Mesa mesa = new Mesa();
+        mesa.setId(mesaDTO.getId().intValue());
+        mesa.setNumMesa(mesaDTO.getNumMesa());
+        mesa.setEstado(mesaDTO.getEstado());
+        mesa.setFechaHora(mesaDTO.getFechaHora());
+        return mesa;
+    }
+}
